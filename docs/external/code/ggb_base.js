97c0c3989dbd84603dbac2bed8c69ab1da5dbc9a
William Haynes <whaynes@maritime.edu>
2022-08-11 17:48:06 -0400

    Publish html and pdf

--- docs/external/code/ggb_base.js
+++ docs/external/code/ggb_base.js
@@ -2,7 +2,7 @@ var listeners = function(ggb) {
 
     var updateSlate = function() {
       node = document.getElementById('info');
-      latex = "$${ " + ggb.getValueString('latex') + "}$$";
+      latex = "\\[{ " + ggb.getValueString('latex') + "}\\]";
       node.innerHTML =  latex ;
       if (window.MathJax) {
          MathJax.typesetPromise([node]).then(() => {});
