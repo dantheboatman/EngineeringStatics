7423a2bd9df6adb3ef0470ce08eec6fa025da503
William Haynes <whaynes@maritime.edu>
2023-01-21 14:27:59 -0500

    Remove ‘Show Reactions’ buttons from Chapter 5 interactives.

--- source/resources/_code/ggb_base_ch5.js
+++ source/resources/_code/ggb_base_ch5.js
@@ -2,13 +2,10 @@ var listeners = function(ggb) {
     const nodeL1 = document.getElementById('L1');
     const nodeR1 = document.getElementById('R1');
 
-
     function toggleVisibility() {
-        nodeR1.style.display = nodeR1.style.display == "none" ? "block" : "none";
-        ggb.setValue('show', nodeR1.style.display == "none" ? false : true);
+        var show = ggb.getValue('show');
+        nodeR1.style.display = show ? "block" : "none";
     }
-
-
 
     var updateSlate = function() {
         nodeL1.innerHTML = "\\[ \\begin{align} \\textcolor{#E12020}" + ggb.getValueString('textL1') + 
@@ -16,15 +13,16 @@ var listeners = function(ggb) {
         nodeR1.innerHTML = "\\[ \\begin{align} \\textcolor{#4191D9}" + ggb.getValueString('textR1') + 
         "\\\\" + "\\textcolor{#4191D9}" + ggb.getValueString('textR2') + " \\end{align} \\]";
         
+        var show = ggb.getValue('show');
+        nodeR1.style.display = show ? "block" : "none";
+        
         if (window.MathJax) {
             MathJax.typesetPromise([ nodeL1,  nodeR1 ]).then(() => {});
         }
     }
     ggb.registerUpdateListener(updateSlate);
     updateSlate();
-    document.getElementById('showReactions').addEventListener('click', function() {
-        toggleVisibility()
-    });
+        
     nodeR1.style.display = "none"
-    nodeR2.style.display = "none"
+
 }
