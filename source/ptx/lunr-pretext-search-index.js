6f848760626cac5fb62e6834e636f2ffe59b29a6
William Haynes <whaynes@maritime.edu>
2023-07-05 11:20:50 -0400

    Whitespace:  Remove linebreaks, spaces to tabs
    Hoping to make git diffs more useful

---
+++ source/ptx/lunr-pretext-search-index.js
@@ -0,0 +1,14 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
