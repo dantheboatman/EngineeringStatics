eb342bbf890ba5ef23f68eb65b8efcc95ae508b5
William Haynes <whaynes@maritime.edu>
2026-02-20 09:08:08 -0500

    Update CLI to 2.37.1

--- .devcontainer/installPandoc.sh
+++ .devcontainer/installPandoc.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-# This file was automatically generated with PreTeXt 2.29.2.
+# This file was automatically generated with PreTeXt 2.37.1.
 # If you modify this file, PreTeXt will no longer automatically update it.
 
-wget https://github.com/jgm/pandoc/releases/download/3.6.4/pandoc-3.6.4-1-amd64.deb -O pandoc.deb
+# Detect architecture and download appropriate Pandoc version
+if uname -m | grep -q "aarch64\|arm64"; then
+    # ARM architecture
+    wget https://github.com/jgm/pandoc/releases/download/3.8.3/pandoc-3.8.3-1-arm64.deb -O pandoc.deb
+else
+    # x86/amd64 architecture
+    wget https://github.com/jgm/pandoc/releases/download/3.8.3/pandoc-3.8.3-1-amd64.deb -O pandoc.deb
+fi
 
 # wait for 60 second and then double check that no other script is using apt-get:
 sleep 60
@@ -12,6 +19,6 @@ while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do
     sleep 15
 done
 # Install pandoc
-sudo apt-get install -y --no-install-recommends ./pandoc.deb 
+apt-get install -y --no-install-recommends ./pandoc.deb 
 
 rm pandoc.deb
