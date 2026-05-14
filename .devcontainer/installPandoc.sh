6bf57bc8854cbd896d8c6fec7b75e955a00c3ce8
William Haynes <whaynes@maritime.edu>
2026-05-14 09:58:46 -0400

    Update CLI to 2.39.0

--- .devcontainer/installPandoc.sh
+++ .devcontainer/installPandoc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This file was automatically generated with PreTeXt 2.37.1.
+# This file was automatically generated with PreTeXt 2.39.0.
 # If you modify this file, PreTeXt will no longer automatically update it.
 
 # Detect architecture and download appropriate Pandoc version
